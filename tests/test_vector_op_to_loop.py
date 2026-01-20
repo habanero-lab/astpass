80f1f3a07cf549c66f3be27d3a24947e779caab4
Tong Zhou <zt9465@gmail.com>
2026-01-20 18:34:01 +0800

    test: add test_vector_op_to_loop.py::test_add4

--- tests/test_vector_op_to_loop.py
+++ tests/test_vector_op_to_loop.py
@@ -60,3 +60,22 @@ def test_add3():
     """
     new_code = ast.unparse(tree)
     assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
+
+def test_add4():
+    code = """
+    c[:] = a[:] * 2 + b
+    """
+    tree = ast.parse(textwrap.dedent(code))
+    rt_vals = {
+        'a': np.random.randn(10),
+        'b': 1.0,
+        'c': np.empty(10)
+    }
+    tree = vector_op_to_loop.transform(tree, rt_vals)
+
+    expected = """
+    for __i0 in range(0, 10):
+        c[__i0] = a[__i0] * 2 + b
+    """
+    new_code = ast.unparse(tree)
+    assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
