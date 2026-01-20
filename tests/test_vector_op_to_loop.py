d2cbaa97a04f05011c36978bd9e61c75d1f0fa9e
Tong Zhou <zt9465@gmail.com>
2026-01-20 18:31:22 +0800

    refactor: rename array_expr_to_loop to vector_op_to_loop

--- tests/test_vector_op_to_loop.py
+++ tests/test_vector_op_to_loop.py
@@ -2,7 +2,7 @@ import ast
 import textwrap
 import numpy as np
 
-from astpass.passes import array_expr_to_loop
+from astpass.passes import vector_op_to_loop
 
 def test_add1():
     code = """
@@ -14,7 +14,7 @@ def test_add1():
         'b': 1.0,
         'c': np.empty(10)
     }
-    tree = array_expr_to_loop.transform(tree, rt_vals)
+    tree = vector_op_to_loop.transform(tree, rt_vals)
 
     expected = """
     for __i0 in range(0, 10):
@@ -33,7 +33,7 @@ def test_add2():
         'b': 1.0,
         'c': np.empty(10)
     }
-    tree = array_expr_to_loop.transform(tree, rt_vals)
+    tree = vector_op_to_loop.transform(tree, rt_vals)
 
     expected = """
     for __i0 in range(0, 10):
@@ -52,31 +52,11 @@ def test_add3():
         'b': 1.0,
         'c': np.empty(10)
     }
-    tree = array_expr_to_loop.transform(tree, rt_vals)
+    tree = vector_op_to_loop.transform(tree, rt_vals)
 
     expected = """
     for __i0 in range(0, 10):
         c[__i0] = a[__i0] + b
-    """
-    new_code = ast.unparse(tree)
-    assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
-
-def test_add4():
-    code = """
-    c = a + b
-    """
-    tree = ast.parse(textwrap.dedent(code))
-    rt_vals = {
-        'a': np.random.randn(3, 4),
-        'b': 1.0,
-        'c': np.empty((3, 4))
-    }
-    tree = array_expr_to_loop.transform(tree, rt_vals)
-
-    expected = """
-    for __i0 in range(0, 3):
-        for __i1 in range(0, 4):
-            c[__i0, __i1] = a[__i0, __i1] + b
     """
     new_code = ast.unparse(tree)
     assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
