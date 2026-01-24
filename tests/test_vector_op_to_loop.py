685a1da6903c8e5b6ae92b4592185e2663313414
Tong Zhou <zt9465@gmail.com>
2026-01-24 13:51:31 +0800

    test: add test_np_min and max

--- tests/test_vector_op_to_loop.py
+++ tests/test_vector_op_to_loop.py
@@ -120,3 +120,43 @@ def test_np_sum2():
     """
     new_code = ast.unparse(tree)
     assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
+
+def test_np_max1():
+    code = """
+    c = np.max(a)
+    """
+    tree = ast.parse(textwrap.dedent(code))
+    rt_vals = {
+        'a': np.random.randn(10),
+        'c': 0.0,
+        'np': np
+    }
+    tree = vector_op_to_loop.transform(tree, rt_vals)
+
+    expected = """
+    c = float('-inf')
+    for __i0 in range(0, 10):
+        c = max(c, a[__i0])
+    """
+    new_code = ast.unparse(tree)
+    assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
+
+def test_np_min1():
+    code = """
+    c = np.min(a)
+    """
+    tree = ast.parse(textwrap.dedent(code))
+    rt_vals = {
+        'a': np.random.randn(10),
+        'c': 0.0,
+        'np': np
+    }
+    tree = vector_op_to_loop.transform(tree, rt_vals)
+
+    expected = """
+    c = float('inf')
+    for __i0 in range(0, 10):
+        c = min(c, a[__i0])
+    """
+    new_code = ast.unparse(tree)
+    assert new_code == ast.unparse(ast.parse(textwrap.dedent(expected)))
